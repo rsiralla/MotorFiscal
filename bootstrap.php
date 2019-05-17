0af9f26a4eac2eb979b5629e2ecb9eb10a22e924
j.fischer <j.fischer@gambio.de>
2019-05-17 16:40:36 +0200

    Ajustando integração continua

--- bootstrap.php
+++ bootstrap.php
@@ -5,5 +5,5 @@
  * Date: 27.03.19
  * Time: 14:24.
  */
-require_once __DIR__.'/../vendor/autoload.php';
-require_once __DIR__.'/Trib.php';
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/tests/Trib.php';
